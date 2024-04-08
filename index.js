6653e9dd51fbd0881702591031435e56b45e8ccc
Ангелина Месяцына <angelmesss13@gmail.com>
2024-04-08 10:17:01 +0300

    2

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 import fs from 'fs';
 import csv from 'csv-parser';
 
-function countCars(file) {
-  let count = 0;
-  fs.createReadStream(file)
-    .pipe(csv())
-    .on('data', () => {
-      count += 1;
-    })
-    .on('end', () => {
-      console.log(`Количество автомобилей: ${count}`);
-    });
+//function countCars(file) {
+  //let count = 0;
+  //fs.createReadStream(file)
+    //.pipe(csv())
+    //.on('data', () => {
+      //count += 1;
+    //})
+    //.on('end', () => {
+      //console.log(`Количество автомобилей: ${count}`);
+    //});
+//}
+
+//export default countCars;
+
+
+function calculateAv(file) {
+    let totalMileage = 0;
+    let carCount = 0;
+
+    fs.createReadStream(file)
+        .pipe(csv())
+        .on('data', (row) => {
+            totalMileage += parseInt(row.mileage);
+            carCount++;
+        })
+        .on('end', () => {
+            const averageMileage = Math.round(totalMileage / carCount);
+            console.log(`Средний пробег: ${averageMileage}`);
+        });
 }
 
-export default countCars;
+export default calculateAv;
