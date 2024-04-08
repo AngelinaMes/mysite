03ac009c0ea85c6028c92708181299566010fbb6
Ангелина Месяцына <angelmesss13@gmail.com>
2024-04-08 10:03:44 +0300

    1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 import _ from 'lodash';
+
+import { showInfo } from '../index.js';
+
+
+function count(file) {
+    let count = 0;
+    fs.createRead(file)
+        .pipe(csv())
+        .on('data', () => {
+            count = count + 1
+        })
+        .on ('end', () => {
+            console.log(`Количество автомобилей: ${count}`)
+        });
+}
+
+
