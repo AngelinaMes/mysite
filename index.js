d62620cd609b5400b0c6edcb0d95b3a32a3ccfcd
Ангелина Месяцына <angelmesss13@gmail.com>
2024-04-08 10:19:05 +0300

    2

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 import fs from 'fs';
 import csv from 'csv-parser';
 
-//function countCars(file) {
-  //let count = 0;
-  //fs.createReadStream(file)
-    //.pipe(csv())
-    //.on('data', () => {
-      //count += 1;
-    //})
-    //.on('end', () => {
-      //console.log(`Количество автомобилей: ${count}`);
-    //});
-//}
+// function countCars(file) {
+// let count = 0;
+// fs.createReadStream(file)
+// .pipe(csv())
+// .on('data', () => {
+// count += 1;
+// })
+// .on('end', () => {
+// console.log(`Количество автомобилей: ${count}`);
+// });
+// }
 
-//export default countCars;
-
+// export default countCars;
 
 function calculateAv(file) {
-    let totalMileage = 0;
-    let carCount = 0;
+  let totalMileage = 0;
+  let carCount = 0;
 
-    fs.createReadStream(file)
-        .pipe(csv())
-        .on('data', (row) => {
-            totalMileage += parseInt(row.mileage);
-            carCount++;
-        })
-        .on('end', () => {
-            const averageMileage = Math.round(totalMileage / carCount);
-            console.log(`Средний пробег: ${averageMileage}`);
-        });
+  fs.createReadStream(file)
+    .pipe(csv())
+    .on('data', (row) => {
+      totalMileage += parseInt(row.mileage, 10);
+      carCount += 1;
+    })
+    .on('end', () => {
+      const averageMileage = Math.round(totalMileage / carCount);
+      console.log(`Средний пробег: ${averageMileage}`);
+    });
 }
 
 export default calculateAv;
